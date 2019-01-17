6bb0d64dbe326fd6957721d854c65ddb155d2d68
Michael Milton <ttmigueltt@gmail.com>
2019-01-17 17:03:43 +1100

    Fixed zoom

--- src/components/Cola.vue
+++ src/components/Cola.vue
@@ -5,11 +5,11 @@
 </template>
 
 <script>
-    import * as d3 from 'd3'
-    import * as cola from 'webcola'
+    import * as d3 from "d3"
+    import * as cola from "webcola"
 
     export default {
-        name: 'Cola',
+        name: "Cola",
         props: {
             nodes: Array,
             links: Array,
@@ -20,72 +20,82 @@
         },
         watch: {
             nodes() {
-                console.log('Updated!')
+                console.log("Updated!")
                 const width = 960, height = 500
-                var margin = 6, pad = 8
+                const multiplier = 0.5
+                const margin = 6 * multiplier
+                const pad = 8 * multiplier
 
                 // graph.nodes.forEach(function (v) { v.height = v.width = 2 * nodeRadius; });
-                var color = d3.scaleOrdinal(d3.schemeCategory20)
+                const color = d3.scaleOrdinal(d3.schemeCategory20)
 
                 const d3cola = cola.d3adaptor(d3)
                     .avoidOverlaps(true)
                     .nodes(this.nodes)
                     .links(this.links)
-                    .flowLayout('y', 60)
-                    .symmetricDiffLinkLengths(60)
+                    // this controls this height of the graph (vertical spacing)
+                    .flowLayout("y", 100 * multiplier)
+                    // this controls this width of the graph (horizontal spacing)
+                    .symmetricDiffLinkLengths(60 * multiplier)
                     .convergenceThreshold(1e-3)
                     .size([width, height])
                     .start(10, 20, 20)
 
                 const svg = d3.select(this.$refs.svg)
-                    .attr('width', width)
-                    .attr('height', height)
+                    .attr("width", width)
+                    .attr("height", height)
+
+                const group = svg.append("g")
+                    .call(d3.zoom().on("zoom", function () {
+                        console.log("zoom")
+                        group.attr("transform", d3.event.transform)
+                    }))
 
                 // define arrow markers for graph links
-                svg.append('svg:defs').append('svg:marker')
-                    .attr('id', 'end-arrow')
-                    .attr('viewBox', '0 -5 10 10')
-                    .attr('refX', 6)
-                    .attr('markerWidth', 3)
-                    .attr('markerHeight', 3)
-                    .attr('orient', 'auto')
-                    .append('svg:path')
-                    .attr('d', 'M0,-5L10,0L0,5')
-                    .attr('fill', '#000')
+                svg.append("svg:defs").append("svg:marker")
+                    .attr("id", "end-arrow")
+                    .attr("viewBox", "0 -5 10 10")
+                    .attr("refX", 6)
+                    .attr("markerWidth", 3)
+                    .attr("markerHeight", 3)
+                    .attr("orient", "auto")
+                    .append("svg:path")
+                    .attr("d", "M0,-5L10,0L0,5")
+                    .attr("fill", "#000")
 
-                const path = svg.selectAll('.link')
+                const path = group.selectAll(".link")
                     .data(this.links)
                     .enter()
-                    .append('svg:path')
-                    .attr('class', 'link')
+                    .append("svg:path")
+                    .attr("class", "link")
 
-                const node = svg.selectAll('.node')
+                const node = group.selectAll(".node")
                     .data(this.nodes)
                     .enter()
-                    .append('rect')
-                    .attr('class', 'node')
-                    .style('stroke', function (d) {
+                    .append("rect")
+                    .attr("class", "node")
+                    .style("stroke", function (d) {
                         return color(d.group)
                     })
                     .call(d3cola.drag)
 
-                var label = svg.selectAll('.label')
+                const label = group.selectAll(".label")
                     .data(this.nodes)
                     .enter()
-                    .append('text')
-                    .attr('class', 'label')
+                    .append("text")
+                    .attr("class", "label")
                     .text(function (d) {
                         return d.name
                     })
                     .call(d3cola.drag)
                     .each(function (d) {
-                        var b = this.getBBox()
-                        var extra = 2 * margin + 2 * pad
+                        const b = this.getBBox()
+                        const extra = 2 * margin + 2 * pad
                         d.width = b.width + extra
                         d.height = b.height + extra
                     })
 
-                var lineFunction = d3.line()
+                const lineFunction = d3.line()
                     .x(function (d) {
                         return d.x
                     })
@@ -93,31 +103,40 @@
                         return d.y
                     })
 
-                d3cola.on('tick', function () {
+                const routeEdges = function () {
+                    d3cola.prepareEdgeRouting()
+                    path.attr("d", function (d) {
+                        return lineFunction(d3cola.routeEdge(d))
+                    })
+                }
+
+
+                d3cola.on("tick", function () {
                     node.each(function (d) {
                         d.innerBounds = d.bounds.inflate(-margin)
-                    }).attr('x', function (d) {
+                    }).attr("x", function (d) {
                         return d.innerBounds.x
-                    }).attr('y', function (d) {
+                    }).attr("y", function (d) {
                         return d.innerBounds.y
-                    }).attr('width', function (d) {
+                    }).attr("width", function (d) {
                         return d.innerBounds.width()
-                    }).attr('height', function (d) {
+                    }).attr("height", function (d) {
                         return d.innerBounds.height()
                     })
 
-                    path.attr('d', function (d) {
-                        var route = cola.makeEdgeBetween(d.source.innerBounds, d.target.innerBounds, 5)
+                    path.attr("d", function (d) {
+                        const route = cola.makeEdgeBetween(d.source.innerBounds, d.target.innerBounds, 5)
                         return lineFunction([route.sourceIntersection, route.arrowStart])
                     })
 
 
-                    label.attr('x', function (d) {
+                    label.attr("x", function (d) {
                         return d.x
-                    }).attr('y', function (d) {
+                    }).attr("y", function (d) {
                         return d.y + (margin + pad) / 2
                     })
-                })
+                }).on("end", routeEdges)
+
             }
         }
     }
@@ -168,7 +187,7 @@
     }
 
     #cola-svg {
-        height: 80%;
+        height: 95%;
         width: 80%
     }
 </style>
